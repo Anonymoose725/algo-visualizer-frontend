4cd00c062d4f6ed99d1bd75910f74d22cb895136
Anonymoose725 <ethangat725@gmail.com>
2026-05-21 18:22:19 -0400

    dijkstra demo compatability

--- src/demo/demoData.js
+++ src/demo/demoData.js
@@ -3,12 +3,14 @@ import mergeSteps from "./merge.json"
 import insertionSteps from "./insertion.json"
 import bstInsert from "./bst-insert.json"
 import bstSearch from "./bst-search.json"
+import dijkstra from "./dijkstra.json"
 
 export const demoData = {
     bubble: { steps: bubbleSteps, input: "8,3,6,1,5,2,7,4" },
     merge: { steps: mergeSteps, input: "8,3,6,1,5,2,7,4" },
     insertion: { steps: insertionSteps, input: "8,3,6,1,5,2,7,4" },
     "bst-insert": { response: bstInsert, input: "5,3,7,1,4,6,8" },
-    "bst-search": { response: bstSearch, input: "5,3,7,1,4,6,8", target: "4" }
+    "bst-search": { response: bstSearch, input: "5,3,7,1,4,6,8", target: "4" },
+    "dijkstra": { response: dijkstra }
 }
 
